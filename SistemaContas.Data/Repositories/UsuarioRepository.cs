0a69fccbc37e47ee08d5b85978ffdc6fc5872aaa
Abrahão Neto <abrahao-neto@outlook.com>
2023-02-11 21:06:01 -0300

    Estruturando a camada de Data, e cadastrando usuario no Banco de Dados

---
+++ SistemaContas.Data/Repositories/UsuarioRepository.cs
@@ -0,0 +1,45 @@
+﻿using Dapper;
+using SistemaContas.Data.Configurations;
+using SistemaContas.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaContas.Data.Repositories
+{
+    /// <summary>
+    /// Repositório de dados para usúario
+    /// </summary>
+    public class UsuarioRepository
+    {
+        /// <summary>
+        /// Método para inserir um usuário no banco de dados
+        /// </summary>
+        /// <param name="usuario"></param>
+        public void Inserir(Usuario usuario)
+        {
+            var query = @"
+                INSERT INTO USUARIO(
+                    IDUSUARIO,
+                    NOME,
+                    EMAIL,
+                    SENHA,
+                    DATAHORACRICAO)
+                VALUES(
+                    @IdUsuario,
+                    @Nome,
+                    @Email,
+                    CONVERT(VARCHAR(32), HASHBYTES('MD5', @Senha), 2),
+                    @DataHoraCriacao)
+            ";
+
+            using (var connection = new SqlConnection(SqlServerConfiguration.ConnectionString))
+            {
+                connection.Execute(query, usuario);
+            }
+        }
+    }
+}
--- SistemaContas.Presentation/Controllers/AccountController.cs
+++ SistemaContas.Presentation/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SistemaContas.Data.Entities;
+using SistemaContas.Data.Repositories;
 using SistemaContas.Presentation.Models;
 
 namespace SistemaContas.Presentation.Controllers
@@ -37,6 +39,29 @@ namespace SistemaContas.Presentation.Controllers
         [HttpPost]
         public IActionResult Register(AccountRegisterModel model)
         {
+            //Verificando se os campos do formulario (model) passaram nas regras de validação mapeadas na classe
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var usuario = new Usuario();
+                    usuario.IdUsuario = Guid.NewGuid();
+                    usuario.Nome = model.Nome;
+                    usuario.Email = model.Email;
+                    usuario.Senha = model.Senha;
+                    usuario.DataHoraCriacao = DateTime.Now;
+
+                    var usuarioRepository = new UsuarioRepository();
+                    usuarioRepository.Inserir(usuario);
+
+                    TempData["Mensagem"] = "Parabéns, sua conta de usuário foi criada com sucesso!";
+
+                }
+                catch (Exception e)
+                {
+                    TempData["Mensagem"] = "Falha ao cadastrar usuário: " + e.Message;
+                }
+            }
             return View();
         }
 
